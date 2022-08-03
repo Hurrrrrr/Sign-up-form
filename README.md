e81fc8f09e00f53729eb3d3f6b0a028232bfc9ae
Hurrrrrr <jonahpalmer@gmail.com>
2022-08-03 16:37:34 -0400

    finish JS

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Sign-up-form
-Sample sign-up form using forms and form validation
+Sample sign-up form using forms and form validation. Intent is to practice basic form styling and client-side validation.
 
 Image credit: Photo by Ben Wicks on Unsplash https://unsplash.com/photos/Ns96HhR-b-c
--- script.js
+++ script.js
@@ -2,11 +2,17 @@
 
 const form = document.getElementById("user_form");
 
+// When the user clicks the submit button, if the passwords do not match
+// do not allow the form to submit
 form.addEventListener("submit", (e) => {
     let checker = false;
     checker = passCompare();
     if (!checker) {
         e.preventDefault();
+        showWarning();
+    }
+    if (checker) {
+        hideWarning();
     }
 })
 
@@ -19,4 +25,14 @@ function passCompare() {
     }
     alert("Success!");
     return true;
+}
+
+function showWarning() {
+    let warn = document.getElementById("pw_warning");
+    warn.textContent = "Passwords do not match!";
+}
+
+function hideWarning() {
+    let hide = document.getElementById("pw_warning");
+    hide.textContent = "";
 }
